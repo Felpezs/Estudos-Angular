5d87bed0e39226c833867acb990b3dd6df8730ea
Felipe Eduardo <felipe.edfreire@gmail.com>
2023-04-12 09:03:39 -0300

    exports e iniciando projeto pokedex

---
+++ Javascript/Fundamentos/exports/imports.js
@@ -0,0 +1,5 @@
+function sum(val_1, val_2){
+    return val_1 + val_2
+}
+
+module.exports = { sum }
---
+++ Javascript/Fundamentos/exports/main.js
@@ -0,0 +1,7 @@
+/*
+    const functions = require('./imports')
+    console.log(functions.sum(3,3))
+*/
+
+const { sum } = require('./imports')
+console.log(sum(3,3))
